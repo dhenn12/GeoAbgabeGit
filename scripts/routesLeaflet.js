1fee70e17c9c7f9824f3bd9c1c368c38efadaa2b
Dorian <jabbatatooine@web.de>
2019-09-09 21:48:06 +0200

    ---

--- scripts/routesLeaflet.js
+++ scripts/routesLeaflet.js
@@ -63,7 +63,7 @@ function createRoute(mapdiv, inputCoords){
   //create a leaflet object from the given coordinates and colors
   var newRoute = new L.GeoJSON(geoJSON);
   newRoute._id = id;
-  newRoute.bindPopup('hello');
+  newRoute.bindPopup("the coordinates are: " +  JSON.stringify(geoJSON.features[0].geometry.coordinates));
   //add the shape to the map and the shape array.
   map.addLayer(newRoute);
   shapes.push(newRoute);
